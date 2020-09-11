55c2a6e937f03a984e30f794eb87b0e349b3489c
Rahul Krishna <rahulkrishna.web@gmail.com>
2020-09-12 03:39:42 +0530

    Added Basic Components
    Addded
    -Material UI
    -Firebase
    -HomePage Skeleton
    -Navbar Skeleton

---
+++ src/pages/homePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainAppBar from "../components/mainAppBar";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <MainAppBar />
+      Hello World!
+    </div>
+  );
+}
+
+export default HomePage;
